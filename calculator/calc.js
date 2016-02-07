d9dfe0b575db9a1d26472874ba3ac75b16e82d20
yermakova_l <yermakova@itstep.org>
2016-02-07 13:28:46 +0000

    second unfinished half working version

--- calculator/calc.js
+++ calculator/calc.js
@@ -7,8 +7,13 @@ var Calc = {
     settings: function(){
         this.input = '';
         this.result = 0;
-        this.arg = '';
+        this.firstArg = 0;
+        this.secondArg = '';
+        this.firstOperator = '';
+        this.argCount = 0;
+        this.temp = '';
         this.isLastDigit = false;
+        this.isLastOperator = false;
         
     },
     
@@ -35,41 +40,36 @@ var Calc = {
             
             case 'CL': this.settings(); this.renderResult(); break;
             
-            case '+':
-                if(this.isLastDigit){
-                    this.result += parseFloat(this.arg);
+            case '+': case '-': case '*': case '/':
+                if(this.isLastDigit && this.argCount == 0){
+                    this.firstOperator = this.input;
+                    this.firstArg = parseFloat(this.temp);
+                    this.temp = '';
+                    this.argCount = 1;
                     this.isLastDigit = false;
-                    this.renderResult();
-                }
-                break;
-                
-            case '-': 
-                if(this.isLastDigit){
-                    this.result -= parseFloat(this.arg);
-                    this.isLastDigit = false;
-                    this.renderResult();
-                }
-                break;
-
-            case '*': 
-                if(this.result == 0){
-                        this.result = 1;
-                } 
-                if(this.isLastDigit){
+                    this.isLastOperator = true;
+                }else if(this.isLastDigit && this.argCount == 1){
+                    this.secondArg = parseFloat(this.temp);
+                    this.temp = '';
                     
-                    this.result *= parseFloat(this.arg);
-                    this.isLastDigit = false;
-                    this.renderResult();
-                }
-                break;
-            case '/':
-                if(this.result == 0){
-                        this.result = 1;
-                } 
-                if(this.isLastDigit){
+                    this.calculate(this.firstArg, this.firstOperator,this.secondArg);
                     
-                    this.result /= parseFloat(this.arg);
+                    this.firstOperator = this.input;
+                    this.firstArg = this.result;
                     this.isLastDigit = false;
+                    this.isLastOperator = true;
+                    this.argCount = 2;
+                    this.renderResult();
+                }else if(this.isLastDigit && this.argCount == 2){
+                    this.secondArg = parseFloat(this.temp);
+                    this.temp = '';
+                    
+                    this.calculate(this.firstArg, this.firstOperator,this.secondArg);
+                    
+                    this.firstOperator = this.input;
+                    this.firstArg = this.result;
+                    this.isLastDigit = false;
+                    this.isLastOperator = true;
                     this.renderResult();
                 }
                 break;
@@ -77,25 +77,32 @@ var Calc = {
             case '0':case '1':case '2':case '3':case '4':
             case '5':case '6':case '7':case '8':case '9':case '.':
                 
-                if(!this.isLastDigit){
+                if(!this.isLastDigit && !this.isLastOperator && this.argCount == 0){
+                    this.temp = this.input;
+                    this.isLastDigit = true;
+                    //this.result = parseFloat(this.temp);
+                    this.renderArgument();
                     
-                    this.arg = this.input;
+                }else if(this.isLastDigit && !this.isLastOperator && this.argCount == 0){
+                    
+                    this.temp += this.input;
+                    //this.result = parseFloat(this.temp);
                     this.isLastDigit = true;
                     this.renderArgument();
-                }else if(this.isLastDigit){
-                    
-                    this.arg += this.input;
+                
+                }else if(this.isLastOperator && this.argCount == 1){
+                    this.temp += this.input;
                     this.isLastDigit = true;
+                   // this.isLastOperator = false;
+                    this.renderArgument();
+                }else if(this.isLastOperator && this.argCount == 2){
+                    this.temp += this.input;
+                    this.isLastDigit = true;
+                   // this.isLastOperator = false;
                     this.renderArgument();
                 }
-                
                 break;
-                
-                
-                
-                 
-            
-            
+
         }
     },
     
@@ -104,7 +111,36 @@ var Calc = {
     },
     
     renderArgument: function () {
-        this.resultField.innerHTML= this.arg;
+        this.resultField.innerHTML= this.temp;
+    },
+    
+    calculate: function( arg1, oper, arg2){
+        
+        switch(oper){
+            
+            case '+':
+                this.result =  arg1 + arg2;
+                break;
+                
+            case '-':
+                this.result =  arg1 - arg2;
+                break;
+                
+            case '*':
+               this.result =  arg1 * arg2;
+                break;
+                
+            case '/':
+                if(this.arg2 !== 0 )
+                    this.result = arg1 / arg2;
+                
+                break;
+            
+            
+            
+            
+        }
+        
     }
 };
 
